065e30dca26189a2837cab766d7db9b920848f5e
Ali Rahimi <1aliirahimi1@gmail.com>
2022-06-09 20:18:32 +0430

    adding some containers to home screen

--- wallet_app/lib/screens/home_screen.dart
+++ wallet_app/lib/screens/home_screen.dart
@@ -1,11 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:wallet_app/models/data.dart';
 
+import '../models/category.dart';
 import '../widgets/chart.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  _buildCategory(Category category , double total){
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal:20 , vertical: 10 ),
+      height: 100,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: Colors.grey[900],
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.shade800,
+            offset: const Offset(0, 2),
+            blurRadius: 6
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(category.name, style: const TextStyle(color: Colors.grey,fontSize: 20, fontWeight: FontWeight.bold),),
+                Text('\$${(category.maxPrice - total).toStringAsFixed(2)} / \$${category.maxPrice}',
+                  style: const TextStyle(color: Colors.grey, fontSize: 16, fontWeight: FontWeight.w600),)
+              ],
+            ),
+          )
+        ],
+        
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,25 +71,38 @@ class HomeScreen extends StatelessWidget {
       body:  CustomScrollView(
         slivers: [
           SliverList(delegate: SliverChildBuilderDelegate((context, index) {
-            return Container(
-              margin: const EdgeInsets.all(8),
-              decoration:   BoxDecoration(
-                boxShadow:  [
-                  BoxShadow(
-                    color: Colors.grey.shade900,
-                    offset: const Offset(0, 2),
-                    blurRadius: 6
-                  )
-                ],
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Chart(incomes: weeklySpending,),
-            );
+            if(index == 0 ){
+              return Container(
+                margin: const EdgeInsets.all(8),
+                decoration:   BoxDecoration(
+                  boxShadow:  [
+                    BoxShadow(
+                        color: Colors.grey.shade900,
+                        offset: const Offset(0, 2),
+                        blurRadius: 6
+                    )
+                  ],
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Chart(incomes: weeklySpending,),
+              );
+            }else{
+              final Category category = categories[index - 1];
+              double total = 0;
+
+              for (var element in category.incomes) {
+                total += element.price;
+
+              }
+              return _buildCategory(category, total);
+
+            }
+
 
           },
-            childCount: 1
+            childCount: 1 + categories.length
           )),
-          
+
         ],
 
       ),
